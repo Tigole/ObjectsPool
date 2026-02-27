c5067642a2cb73d8765ca19ccce7b2eb568ee688
JaJaLys <sylvain.janniere@laposte.net>
2026-02-27 14:23:45 +0100

    fix project

--- xmake.lua
+++ xmake.lua
@@ -16,19 +16,18 @@ add_requires("gtest", {system = false, configs = {main = true, gmock = false, sh
 target("op")
     set_kind("headeronly")
     add_headerfiles("include/ObjectsPool.hpp", "include/ObjectsPool.inl", {public=true})
-
+    add_includedirs("include/", {public=true})
 
 -- Iterate over test files and create test targets
 for _, file in ipairs(os.files("tests/*.cpp")) do
     local name = path.basename(file)
     target(name)
         set_kind("binary")
-        add_packages("gtest", {main = true, gmok = false})
         set_default(false)
+        add_packages("gtest", {main = true, gmok = false})
+        add_deps("op")
         add_files(file)
         add_tests("default")
-        add_includedirs("include/", {public = true})
-        --add_deps("op")
         add_includedirs("tests/")
         set_group("test")
 end
@@ -36,11 +35,11 @@ for _, file in ipairs(os.files("tests/build-should-fail/*.cpp")) do
     local name = path.basename(file)
     target(name)
         set_kind("binary")
-        add_packages("gtest", {main = true, gmok = false})
         set_default(false)
+        add_packages("gtest", {main = true, gmok = false})
+        add_deps("op")
         add_files(file)
         add_tests("build-should-fail", {build_should_fail = true})
-        add_includedirs("include/", {public = true})
         add_includedirs("tests/")
         set_group("test")
 end
